c5244e7d78da5b62966a1591a650303ca83c58b5
Dave Greenbaum <zymurgeek@gmail.com>
2023-12-12 21:53:46 -0500

    Refactor isPartNumber

--- day-3-a/src/main/java/com/example/day3a/EngineSchematicLinePartNumberAnalyzer.java
+++ day-3-a/src/main/java/com/example/day3a/EngineSchematicLinePartNumberAnalyzer.java
@@ -9,40 +9,41 @@ public class EngineSchematicLinePartNumberAnalyzer {
     /*
     If a symbol (not a digit or period) exists above, below, to the right, to the left, or diagonally from any
     digit in the number, include the value of the number in the result.
-     */
+    */
     public boolean isPartNumber(MatchResult match, String previousLine, String currentLine, String nextLine) {
 
-        if (match.start() > 0) {
-            if (previousLine != null && isSymbol(previousLine.charAt(match.start() - 1))) {
-                return true;
-            }
-            if (nextLine != null && isSymbol(nextLine.charAt(match.start() - 1))) {
-                return true;
-            }
-            if (isSymbol(currentLine.charAt(match.start() - 1))) {
-                return true;
-            }
-        }
-        if (previousLine != null && match.end() < previousLine.length()
-                && isSymbol(previousLine.charAt(match.end()))) {
-            return true;
-        }
-        if (nextLine != null && match.end() < nextLine.length()
-                && isSymbol(nextLine.charAt(match.end()))) {
-            return true;
-        }
-        if (match.end() < currentLine.length()
-            && isSymbol(currentLine.charAt(match.end()))) {
-            return true;
-        }
-        if (previousLine != null && isSymbol(previousLine.charAt(match.start()))) {
-            return true;
-        }
         if (previousLine != null) {
-            for (int charIndex=match.start(); charIndex < match.end(); ++charIndex) {
+            // Northwest
+            if (match.start() > 0 && isSymbol(previousLine.charAt(match.start() - 1))) {
+                return true;
+            }
+            // North
+            for (int charIndex = match.start(); charIndex < match.end(); ++charIndex) {
                 if (isSymbol(previousLine.charAt(charIndex))) {
                     return true;
                 }
+            }
+            // Northeast
+            if (match.end() < previousLine.length() && isSymbol(previousLine.charAt(match.end()))) {
+                return true;
+            }
+        }
+        // West
+        if (match.start() > 0 && isSymbol(currentLine.charAt(match.start() - 1))) {
+            return true;
+        }
+        // East
+        if (match.end() < currentLine.length() && isSymbol(currentLine.charAt(match.end()))) {
+            return true;
+        }
+        if (nextLine != null) {
+            // Southwest
+            if (match.start() > 0 && isSymbol(nextLine.charAt(match.start() - 1))) {
+                return true;
+            }
+            // Southeast
+            if (match.end() < nextLine.length() && isSymbol(nextLine.charAt(match.end()))) {
+                return true;
             }
         }
         return false;
