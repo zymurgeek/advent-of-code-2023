5bf5aef435ce955ce9c57badc12d39aadbb275a2
Dave Greenbaum <zymurgeek@gmail.com>
2023-12-17 10:53:55 -0500

    Create day-4-a from Spring Initializr

---
+++ day-4-scratchcards/src/main/java/com/example/adventofcode3/day4scratchcards/Day4ScratchcardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventofcode3.day4scratchcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4ScratchcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4ScratchcardsApplication.class, args);
+	}
+
+}
---
+++ day-4-scratchcards/src/test/java/com/example/adventofcode3/day4scratchcards/Day4ScratchcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventofcode3.day4scratchcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day4ScratchcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
