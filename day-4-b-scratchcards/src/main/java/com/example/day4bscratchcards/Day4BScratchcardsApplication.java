ee7a4220a50ec2ad32487db3555263dcd8bfd5ac
Dave Greenbaum <zymurgeek@gmail.com>
2023-12-24 15:02:52 -0500

    Add day-4-b-scratchcards

---
+++ day-4-b-scratchcards/src/main/java/com/example/day4bscratchcards/Day4BScratchcardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day4bscratchcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4BScratchcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4BScratchcardsApplication.class, args);
+	}
+
+}
---
+++ day-4-b-scratchcards/src/test/java/com/example/day4bscratchcards/Day4BScratchcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day4bscratchcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day4BScratchcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
